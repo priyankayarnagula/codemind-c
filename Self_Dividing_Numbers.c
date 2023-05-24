2f9ee092585b66292d278b9b93bd77001a60595f
priyanka yarnagula <123613171+priyankayarnagula@users.noreply.github.com>
2023-05-24 12:44:43 +0530

    #include<stdio.h> int main() {     int x,y,r,c,co,n;     scanf("%d%d",&x,&y);//x=1 y=22     for(x;x<=y;x++)//2<=22     {         n=x;//n=2         co=0;         c=0;         while(n>0)//1>0         {             r=n%10;//r=1             n/=10;//n=0             co++;//co=1             if(r==0)             {             	break; 			}             if(x%r==0)//1%1==0             {                 c++;//c=1             }         }         if(c==co)         {             printf("%d ",x);         }     } }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,29 +1,30 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,x,r;
-	scanf("%d%d",&a,&b);
-	for(a;a<=b;a++)
-	{
-		int count=0;
-		x=a;
-		while(x>0)
-		{
-			r=x%10;
-			if(r==0)
-			{
-				count++;
-				break;
+    int x,y,r,c,co,n;
+    scanf("%d%d",&x,&y);//x=1 y=22
+    for(x;x<=y;x++)//2<=22
+    {
+        n=x;//n=2
+        co=0;
+        c=0;
+        while(n>0)//1>0
+        {
+            r=n%10;//r=1
+            n/=10;//n=0
+            co++;//co=1
+            if(r==0)
+            {
+            	break;
 			}
-			if(a%r!=0)
-			{
-				count++;
-			}
-			x=x/10;
-		}
-		if(count==0)
-		{
-			printf("%d ",a);
-		}
-	}
+            if(x%r==0)//1%1==0
+            {
+                c++;//c=1
+            }
+        }
+        if(c==co)
+        {
+            printf("%d ",x);
+        }
+    }
 }
