4d90cc63f0c237ff718ba615a58d3f6a3042c6b0
priyanka yarnagula <123613171+priyankayarnagula@users.noreply.github.com>
2023-05-24 12:35:59 +0530

    #include<stdio.h> int prime(int n) {     int x=0;     while(n)     {         int c=0;         for(int i=2;i<n/2;i++)         {             if(n%i==0)             c++;         }         if(c==0)         {             return n;         }         n++;     } } int main() {     int a,b;     scanf("%d%d",&a,&b);     printf("%d",prime(a+b+1)-(a+b)); }

--- Battle_of_Primes.c
+++ Battle_of_Primes.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
-int main(){
-	int a,b,c,d,i=1,count=0;
-	scanf("%d %d",&a,&b);
-	c=a+b;
-	while(i<=c){
-		d=c+i;
-		count++;
-		int sum=0,j=1;
-		for(j;j<=d;j++){
-			if(d%j==0){
-				sum++;
-			}
-		}
-		if(sum==2){
-			printf("%d",count);
-			break;
-		}
-		i++;
-	}
+int prime(int n)
+{
+    int x=0;
+    while(n)
+    {
+        int c=0;
+        for(int i=2;i<n/2;i++)
+        {
+            if(n%i==0)
+            c++;
+        }
+        if(c==0)
+        {
+            return n;
+        }
+        n++;
+    }
+}
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",prime(a+b+1)-(a+b));
 }
